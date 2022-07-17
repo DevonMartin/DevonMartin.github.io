b89219e78244884ea2c237a79f6ffdc97cdda2a3
DevonMartin <85790366+DevonMartin@users.noreply.github.com>
2022-07-17 11:22:02 -0400

    Reorganization of Site Files + minor updating

--- js/gallery.js
+++ js/gallery.js
@@ -1,11 +1,11 @@
 function css() {
     document.getElementById("picwheel-header").innerHTML = "HTML";
-    document.getElementById("header-png").setAttribute("src", "header-css.png");
+    document.getElementById("header-png").setAttribute("src", "pics/header-css.png");
     document.getElementById("picwheel-header").setAttribute("onclick", "html()");
 }
 
 function html() {
     document.getElementById("picwheel-header").innerHTML = "CSS";
-    document.getElementById("header-png").setAttribute("src", "header-html.png");
+    document.getElementById("header-png").setAttribute("src", "pics/header-html.png");
     document.getElementById("picwheel-header").setAttribute("onclick", "css()");   
 }
