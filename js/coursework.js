ed86b5a45026d04674e9c996a111e36d2ecaa717
DevonMartin <85790366+DevonMartin@users.noreply.github.com>
2022-07-19 13:30:41 -0400

    Adding better mobile support for coursework table

--- js/coursework.js
+++ js/coursework.js
@@ -1,5 +1,6 @@
 var nextDate = new Date();
 if (nextDate.getMinutes() % 1 === 0) {
+    initMobileTable()
     getCourses()
 } else {
     nextDate.setHours(nextDate.getHours() + 1);
@@ -10,24 +11,49 @@ if (nextDate.getMinutes() % 1 === 0) {
     setTimeout(getCourses, difference);
 }
 
-async function getCourses() {
+function initMobileTable() {
 
+    // Adjust header for mobile
     var x = window.matchMedia("(max-width: 450px)")
-
     if (x.matches) { // If media query matches
+
+        document.getElementById('course-table').innerHTML = `
+            <div class="container-mobile">
+                <div class="course-grid" id="CS50x-header">
+                    CS50x - Introduction to Computer Science
+                    <div id="CS50x-proj"></div>
+                </div>
+                <div class="course-grid" id="CS61A-header">
+                    CS61A - Structure and Interpretation of Computer Programs
+                    <div id="CS61A-proj"></div>
+                </div>
+            </div>
+            <div class="container-mobile">
+                <div class="course-grid" id="CS61B-header">
+                    CS61B - Data Structures
+                    <div id="CS61B-proj"></div>
+                </div>
+                <div class="course-grid" id="CS61C-header">
+                    CS61C - Great Ideas in Computer Architecture
+                    <div id="CS61C-proj"></div>
+                </div>
+            </div>`
+
         document.getElementById("CS50x-header").innerHTML = `CS50x<div id="CS50x-proj"></div>`
         document.getElementById("CS61A-header").innerHTML = `CS61A<div id="CS61A-proj"></div>`
         document.getElementById("CS61B-header").innerHTML = `CS61B<div id="CS61B-proj"></div>`
         document.getElementById("CS61C-header").innerHTML = `CS61C<div id="CS61C-proj"></div>`
     }
+}
+
+async function getCourses() {
 
     // BEGIN CS50x
 
     res = await fetch('https://api.github.com/repos/DevonMartin/CS50x/contents/fin')
     answer = await res.json()
-    x = answer.length
-    CS50len = x
     var CS50html = ``
+    x = answer.length
 
     for (i = 1; i < x; i+=1) {
         CS50html += `<div class="course-grid" style="background-color: green;">` + (answer[i]['name']) + `</div>`
@@ -36,7 +62,6 @@ async function getCourses() {
     res = await fetch('https://api.github.com/repos/DevonMartin/CS50x/contents/inprog')
     answer = await res.json()
     x = answer.length
-    CS50len += x
 
     for (i = 1; i < x; i+=1) {
         CS50html += `<div class="course-grid" style="background-color: yellow;">` + (answer[i]['name']) + `</div>`
@@ -45,7 +70,6 @@ async function getCourses() {
     res = await fetch('https://api.github.com/repos/DevonMartin/CS50x/contents/todo')
     answer = await res.json()
     x = answer.length
-    CS50len += x
 
     for (i = 1; i < x; i+=1) {
         CS50html += `<div class="course-grid" style="background-color: red;">` + (answer[i]['name']) + `</div>`
@@ -57,9 +81,8 @@ async function getCourses() {
     
     res = await fetch('https://api.github.com/repos/DevonMartin/CS61A/contents/fin')
     answer = await res.json()
-    x = answer.length
-    CS61Alen = x
     var CS61Ahtml = ``
+    x = answer.length
 
     for (i = 1; i < x; i+=1) {
         CS61Ahtml += `<div class="course-grid" style="background-color: green;">` + (answer[i]['name']) + `</div>`
@@ -68,7 +91,6 @@ async function getCourses() {
     res = await fetch('https://api.github.com/repos/DevonMartin/CS61A/contents/inprog')
     answer = await res.json()
     x = answer.length
-    CS61Alen += x
 
     for (i = 1; i < x; i+=1) {
         CS61Ahtml += `<div class="course-grid" style="background-color: yellow;">` + (answer[i]['name']) + `</div>`
@@ -77,14 +99,10 @@ async function getCourses() {
     res = await fetch('https://api.github.com/repos/DevonMartin/CS61A/contents/todo')
     answer = await res.json()
     x = answer.length
-    CS61Alen += x
 
     for (i = 1; i < x; i+=1) {
         CS61Ahtml += `<div class="course-grid" style="background-color: red;">` + (answer[i]['name']) + `</div>`
     }
-    // for (i = 0; i < CS50len - CS61Alen; i+=1) {
-    //     CS61Ahtml += `<div class="course-grid" style="border-color:white">&zwnj;</div>`
-    // }
     document.getElementById("CS61A-proj").innerHTML = CS61Ahtml
 
     // END CS61A
